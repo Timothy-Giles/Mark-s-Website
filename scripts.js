ca93d2d5912a0961fa2d0022e61eb44d6e8343f7
Your Name <you@example.com>
2024-11-02 19:42:31 -0500

    First commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Smooth scrolling to make navigation smoother when users click the links in your navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
